69117cb706d44b2bd60e6ea3a672c54fc071e9af
JIAXUAN ZHAO <crystalzhao95@JIAXUANs-MacBook-Air.local>
2025-05-03 08:13:44 -0700

    WIP: setup across all services

--- auth-service/src/main/java/com/forum/authService/Controller/AuthController.java
+++ auth-service/src/main/java/com/forum/authService/Controller/AuthController.java
@@ -1,4 +1,15 @@
 package com.forum.authService.Controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth/test")
 public class AuthController {
+    @GetMapping
+    public String testAuth() {
+        return "✅ Auth Service is working through API Gateway!";
+    }
 }
---
+++ auth-service/src/main/java/com/forum/authService/authServiceApplication.java
@@ -0,0 +1,13 @@
+package com.forum.authService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class authServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(authServiceApplication.class, args);
+    }
+
+}
