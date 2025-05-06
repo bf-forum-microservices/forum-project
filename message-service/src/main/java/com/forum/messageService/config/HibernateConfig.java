5c70e457e63ec69f8061f413baa475a02f25109a
mtnp <Silasp0801@gmail.com>
2025-05-06 00:30:23 -0500

    got a lot of functionality done, mostly done with endpoints

---
+++ message-service/src/main/java/com/forum/messageService/config/HibernateConfig.java
@@ -0,0 +1,66 @@
+package com.forum.messageService.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@EnableAspectJAutoProxy
+@EnableCaching
+public class HibernateConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    protected LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.forum.messageService.entity");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", env.getProperty("spring.jpa.database-platform"));
+        properties.setProperty("hibernate.show_sql", env.getProperty("spring.jpa.show-sql"));
+
+        return properties;
+    }
+}
+
+
+
+
--- message-service/src/main/java/com/forum/messageService/controller/MessageController.java
+++ message-service/src/main/java/com/forum/messageService/controller/MessageController.java
@@ -1,27 +1,64 @@
 package com.forum.messageService.controller;
 
-import com.forum.messageService.dto.MessageResponse;
+import com.forum.messageService.dto.MessageRequest;
+import com.forum.messageService.entity.Message;
+import com.forum.messageService.entity.Message.Status;
+import com.forum.messageService.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/admin/messages")
+@RequestMapping("")
 public class MessageController {
 
-//    @GetMapping("/all")
-//    public ResponseEntity<MessageResponse> getAllMessages() {
-//        return ResponseEntity.
-//    }
+    @Autowired
+    private MessageService messageService;
 
-    @GetMapping("/all")
-    public String getAllMessages() {
-        return "All Messages";
+    @GetMapping("/admin/messages/all")
+    public ResponseEntity<List<Message>> getAllMessages() {
+        List<Message> messages = messageService.getAllMessages();
+        return ResponseEntity.ok(messages);
     }
 
-    @GetMapping("/{id}")
-    public String getMessageById(@PathVariable Integer messageId) {
-        return "Message Id: " + messageId;
+    @GetMapping("/admin/messages/{messageId}")
+    public ResponseEntity<?> getMessageById(@PathVariable("messageId") Integer messageId) {
+        Message message = messageService.getMessageById(messageId);
+        if (message == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(message);
     }
 
+    @PostMapping("/admin/messages/create")
+    public ResponseEntity<String> createMessage(@RequestBody MessageRequest messageRequest) {
+        Message message = Message.builder()
+                .userId(messageRequest.getUserId())
+                .email(messageRequest.getEmail())
+                .subject(messageRequest.getSubject())
+                .message(messageRequest.getMessage())
+                .build();
+        messageService.createMessage(message);
+        return ResponseEntity.ok("Message created successfully");
+    }
+
+    @DeleteMapping("/admin/messages/{messageId}")
+    public ResponseEntity<String> deleteMessage(@PathVariable("messageId") Integer messageId) {
+        messageService.deleteMessage(messageId);
+        return ResponseEntity.ok("Message deleted successfully");
+    }
+
+    @PatchMapping("/admin/messages/{messageId}/status")
+    public ResponseEntity<String> changeStatus(
+            @PathVariable("messageId") Integer messageId,
+            @RequestParam("status") String statusString) {
+        statusString = statusString.toUpperCase();
+        if (statusString.equals("PROCESSED") || statusString.equals("RESOLVED")) {
+            messageService.changeMessageStatus(messageId, Status.valueOf(statusString));
+        }
+
+        return ResponseEntity.ok("Message status updated to: " + statusString);
+    }
 }
