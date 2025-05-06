2fe652f7b7998e70b7032300ef37b5b6ca7f487c
JIAXUAN ZHAO <crystalzhao95@JIAXUANs-MacBook-Air.local>
2025-05-06 15:26:55 -0700

    acutator and swagger

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -1,4 +1,4 @@
 // auth.js
 export function isAuthenticated() {
-    return !!localStorage.getItem('jwtToken'); // or use cookies if that's how you store the JWT
+    return !!localStorage.getItem('token'); // or use cookies if that's how you store the JWT
 }
---
+++ user-service/src/main/java/com/forum/userservice/exception/ForbiddenException.java
@@ -0,0 +1,4 @@
+package com.forum.userservice.exception;
+
+public class ForbiddenException {
+}
