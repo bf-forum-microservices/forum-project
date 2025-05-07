f988650ba05a7ea689985db99fa1477e5ca706bc
mtnp <Silasp0801@gmail.com>
2025-05-07 16:51:29 -0500

    saving changes

--- frontend/src/pages/MessageManagement.js
+++ frontend/src/pages/MessageManagement.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import './Pages.css'
 
 const AdminMessages = () => {
     const [messages, setMessages] = useState([]);
@@ -102,58 +103,3 @@ const AdminMessages = () => {
 
 export default AdminMessages;
 
-
-
-// import React, { useEffect, useState } from 'react';
-//
-// const MessageManagement = () => {
-//     const [messages, setMessages] = useState([
-//         {
-//             id: 1,
-//             from: 'Admin',
-//             subject: 'Welcome to the Forum!',
-//             body: 'Hi there! Thanks for joining. Let us know if you have any questions.',
-//             date: '2025-05-01T09:00:00Z',
-//             read: false
-//         },
-//         {
-//             id: 2,
-//             from: 'Moderator',
-//             subject: 'Your Post Has Been Reviewed',
-//             body: 'Please update your post to follow community guidelines.',
-//             date: '2025-05-02T14:30:00Z',
-//             read: true
-//         },
-//         {
-//             id: 3,
-//             from: 'Admin',
-//             subject: 'Maintenance Notice',
-//             body: 'The forum will be undergoing maintenance on May 10 from 1am to 3am.',
-//             date: '2025-05-04T12:45:00Z',
-//             read: false
-//         }
-//     ]);
-//
-//     const handleMarkAsRead = (id) => {
-//         setMessages(messages.map(msg => msg.id === id ? { ...msg, read: true } : msg));
-//     };
-//
-//     return (
-//         <div className="message-management">
-//             <h2>Inbox</h2>
-//             <ul>
-//                 {messages.map(msg => (
-//                     <li key={msg.id} style={{ marginBottom: '16px', backgroundColor: msg.read ? '#f5f5f5' : '#fff' }}>
-//                         <strong>{msg.subject}</strong> from {msg.from} - {new Date(msg.date).toLocaleString()}<br />
-//                         <p>{msg.body}</p>
-//                         {!msg.read && (
-//                             <button onClick={() => handleMarkAsRead(msg.id)}>Mark as Read</button>
-//                         )}
-//                     </li>
-//                 ))}
-//             </ul>
-//         </div>
-//     );
-// };
-//
-// export default MessageManagement;
