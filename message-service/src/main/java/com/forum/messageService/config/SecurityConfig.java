8d97de13caba39d78d61ffa9247a814cc29f0513
mtnp <Silasp0801@gmail.com>
2025-05-08 06:14:36 -0500

    validation and authorization for message service

--- message-service/src/main/java/com/forum/messageService/config/SecurityConfig.java
+++ message-service/src/main/java/com/forum/messageService/config/SecurityConfig.java
@@ -1,4 +1,40 @@
 package com.forum.messageService.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**").permitAll()
+                .antMatchers("/users/register", "/users/verify-email", "/contact-us", "/auth/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic().disable()
+                .formLogin().disable();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
+
